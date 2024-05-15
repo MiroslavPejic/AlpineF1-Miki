a9b72315a44f9567dcd5c953e265ca02470bb419
MikiPejic <miki.pejic@yahoo.co.uk>
2024-05-15 16:21:40 +0100

    Added viewing of circuit points

---
+++ AlpineF1-Miki/Data/AlpineCircuits.cs
@@ -0,0 +1,24 @@
+﻿namespace AlpineF1_Miki
+{
+    public class AplineCircuits
+    {
+        public int id { get; set; }
+        public string? track_name { get; set; }
+        public string? track_location { get; set; }
+        public string? date_added { get; set; }
+    }
+
+    public class AplineCircuitPoints
+    {
+        public int id { get; set; }
+        public string? track_name { get; set; }
+        public string? track_location { get; set; }
+        public string? circuit_points { get; set; }
+    }
+
+    public class PagedCircuitPoints
+    {
+        public string Points { get; set; }
+        public int TotalPoints { get; set; }
+    }
+}
